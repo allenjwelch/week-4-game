3cf937fafa34aa66cb1e4d54343bfed566c832d6
Allen Welch <allenjwelch@gmail.com>
2018-03-07 17:35:13 -0500

    final -added sound effect

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,8 +14,27 @@ function resetGame() {
     gemNum = []; 
     gemValue = 0; 
     gemImages.unbind('click');
+    $('#gemCounterDisplay').text('--'); //-----Displays on screen
+    $("#win").delay("slow").animate({ opacity: "0"});
+    $("#lose").delay("slow").animate({ opacity: "0"});
     console.log('GAME RESET'); 
     gamePlay(); 
+}; 
+
+function playAudio() {
+  var audio = document.getElementById('audio'); 
+  // if (audio.paused) {
+  //   audio.play(); 
+  //   audio.delay('slow').currentTime = 0; 
+  // } else {
+  //   audio.currentTime = 0; 
+  // }
+  
+  // audio.currentTime = 0; 
+  audio.play(); 
+  // audio.delay('slow').pause(); 
+
+
 }; 
 
 function gamePlay() {
@@ -30,7 +49,9 @@ function gamePlay() {
   for (i = 0; i < 4; i++) {
     gemNum[i] = (Math.floor(Math.random() * 12)+1); 
   } console.log('gemNum', gemNum); //------------Checks for gemNum
-  
+  $('#goalNumberDisplay').text(goalNum); //-----Displays on screen
+
+
     //assign gemNum to crystals
     $('#orangeGem').attr("value", gemNum[0]);
       // orangeValue = $('#orangeGem').attr("value", gemNum[0]);
@@ -41,22 +62,31 @@ function gamePlay() {
   
     //when clicked, add gemNums to userNum
     gemImages.on('click', '.gemImgs', function() {
+      playAudio(); 
+      // document.getElementById('audio').play(); 
       gemValue = ($(this).attr("value"));
       gemValue = parseInt(gemValue);
       console.log('gemValue: ', gemValue);
-  
+      
       counter += gemValue; 
       console.log('counter', counter);
-  
+      $('#gemCounterDisplay').text(counter); //-----Displays on screen
+      
       if (counter === goalNum) {
         userScore++; 
-        alert('You WIN!'); 
-        // gameActive = false; 
+        // alert('You WIN!'); 
+        $("#win").animate({ opacity: "1" });
+
+
+        $('#userScoreDisplay').text(userScore); //-----Displays on screen
         resetGame(); 
   
       } else if (counter > goalNum) {
-        alert('You SUCK!'); 
-        // gameActive = false; 
+        userScore--; 
+        // alert('You SUCK!'); 
+        $("#lose").animate({ opacity: "1"});
+
+        $('#userScoreDisplay').text(userScore); //-----Displays on screen
         resetGame(); 
       }
     }); 
@@ -69,3 +99,25 @@ function gamePlay() {
 }; 
 
 gamePlay(); 
+
+
+// $(".vis-button").on("click", function() {
+//   $(".captain-planet").animate({ opacity: "1" });
+// });
+
+        // // Create an audio element with JavaScript
+        // var audioElement = document.createElement("audio");
+
+        // // Set it's source to the location
+        // // of our Captain Planet theme song file.
+        // audioElement.setAttribute("src", "Assets/captainplanet24.mp3");
+
+        // // Theme Button
+        // $(".theme-button").on("click", function() {
+        //   audioElement.play();
+        // });
+
+        // // Pause Button
+        // $(".pause-button").on("click", function() {
+        //   audioElement.pause();
+        // });
